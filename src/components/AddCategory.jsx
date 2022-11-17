290d6a293bc5b4a4fdb80993f0f511408664b015
Sebastian Letelier <sebalete10@gmail.com>
2022-11-17 10:02:16 -0300

    prueba a AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 
 
@@ -27,4 +28,7 @@ export const AddCategory = ({ onNewCategory }) => {
         />
     </form>
   )
+}
+AddCategory.propTypes = {
+     onNewCategory: PropTypes.func.isRequired,
 }
